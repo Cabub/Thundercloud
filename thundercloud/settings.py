6889248d1a09958250b4debcdacdd01bb54e38b7
cabub <cabub@cabub.me>
2019-10-11 10:53:34 -0500

    updated settings.py

--- thundercloud/settings.py
+++ thundercloud/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'thundercloud.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'thundercloud',
+        'USER': CONFIG['DATABASE_USER'],
+        'PASSWORD': CONFIG['DATABASE_PASSWORD'],
+        'HOST': CONFIG['DATABASE_HOSTNAME'],
+        'PORT': '5432',
     }
 }
 
